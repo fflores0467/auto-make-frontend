da542f67f5f058a33f256b78c04cf887223bf5af
Fernando Flores <fflor@Chocotop>
2024-10-21 01:28:06 -0400

    added dispacter to schedule page to add to the reducer, added inital values to each component in schedule feature

--- src/SetUp/Schedule.tsx
+++ src/SetUp/Schedule.tsx
@@ -1,5 +1,10 @@
 import { Header } from './Header';
 import { useState } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux'
+import { setScheduleInputState } from '../features/schedule/scheduleSlice'
+import type { RootState, AppDispatch } from '../store'
+
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
@@ -8,18 +13,34 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 export const Schedule = () => {
+    const dispatch = useDispatch<AppDispatch>(); 
+    const schedule = useSelector((state: RootState) => state.schedule); 
+
+    type Schema = {
+        name: string;
+        start_date: Date;
+        end_date: Date;
+        interval: number;
+        time_unit: string;
+        specific_time: string;
+        automation_id: number;
+        isContinuous: number;
+    }
+
     type TimeUnit = {
-        unit: String,
-        time_str: String
+        unit: string,
+        time_str: string
     }
-
     const time_units: TimeUnit[] = [{unit: "minutes", time_str: ":SS"}, {unit: "hours", time_str: "MM:SS || :MM"}, {unit: "days", time_str: "HH:MM:SS || HH:MM"}];
-    const [automation, setAutomation] = useState("")
 
-    const handleAutomationChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setAutomation(event.target.value);
-    }
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
 
+        let data: Schema;
+         
+        dispatch(setScheduleInputState({ name, value }));
+      };
+    
     return (
         <Container fluid>
             <Card>
@@ -32,7 +53,12 @@ export const Schedule = () => {
                             <Col md={12}>
                                 <Form.Group>
                                     <Form.Label>Schedule Name</Form.Label>
-                                    <Form.Control placeholder='Enter Schedule Name'></Form.Control>
+                                    <Form.Control 
+                                        placeholder='Enter Schedule Name'
+                                        onChange={handleInputChange} 
+                                        name='name'
+                                        value={schedule.name}
+                                    />
                                 </Form.Group>
                             </Col>
                         </Row>    
@@ -41,13 +67,21 @@ export const Schedule = () => {
                             <Col md={6}>
                                 <Form.Group>
                                     <Form.Label>Start Date</Form.Label>
-                                    <Form.Control type="date"></Form.Control>
+                                    <Form.Control 
+                                        type="date" 
+                                        name='start_date'
+                                        value={schedule.start_date.toISOString().split('T')[0]}
+                                    />
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
                                 <Form.Group>
                                     <Form.Label>End Date</Form.Label>
-                                    <Form.Control type="date"></Form.Control>
+                                    <Form.Control 
+                                        type="date" 
+                                        name='end_date'
+                                        value={schedule.end_date.toISOString().split('T')[0]}
+                                    />
                                 </Form.Group>
                             </Col>
                         </Row>  
@@ -57,40 +91,48 @@ export const Schedule = () => {
                             <Col md={2}>
                                 <Form.Group>
                                     <Form.Label>Every:</Form.Label>
-                                    <Form.Control type="number" value={5}></Form.Control>
+                                    <Form.Control 
+                                        type="number" 
+                                        name='interval'
+                                        value={schedule.interval}
+                                    />
                                 </Form.Group>
                             </Col>
                             <Col md={2}>
                                 <Form.Group>
                                     <Form.Label>Time Unit:</Form.Label>
-                                    <Form.Select style={{textTransform: 'capitalize'}} aria-label="Default select example">
-                                        {time_units.map(x => 
-                                            <option>{x.unit}</option>
-                                        )}
+                                    <Form.Select style={{textTransform: 'capitalize'}} aria-label="Default select example" name='time_unit' value={schedule.time_unit}>
+                                        {time_units.map((x) => (
+                                            <option key={x.unit} value={x.unit}>{x.unit}</option> 
+                                        ))}
                                     </Form.Select>
                                 </Form.Group>
                             </Col>
                             <Col md={2}>
                                 <Form.Group>
                                     <Form.Label>At:</Form.Label>
-                                    <Form.Control placeholder=':SS'></Form.Control>
+                                    <Form.Control 
+                                        placeholder={schedule.specific_time}
+                                        name='specific_time'
+                                        value={schedule.specific_time}
+                                    />
                                 </Form.Group>
                             </Col>
                             <Col md={4}>
                                 <Form.Group>
                                     <Form.Label>Do:</Form.Label>
-                                    <Form.Select onChange={handleAutomationChange} aria-label="Default select example">
-                                        <option>Select Automation...</option> 
-                                        <option>Universal Hotel Finder</option>
+                                    <Form.Select aria-label="Default select example" name='automation_id' value={schedule.automation_id}>
+                                        <option value={0}>Select Automation...</option> 
+                                        <option value={1}>Universal Hotel Finder</option>
                                     </Form.Select>
                                 </Form.Group>
                             </Col>
                             <Col md={2}>
                                 <Form.Group>
                                     <Form.Label>Until:</Form.Label>
-                                    <Form.Select aria-label="Default select example">
-                                        <option>End Date Reached</option>
-                                        <option>Critiria Met</option>
+                                    <Form.Select aria-label="Default select example" name='isContinuous' value={schedule.isContinuous}>
+                                        <option value={0}>End Date Reached</option>
+                                        <option value={1}>Critiria Met</option>
                                     </Form.Select>
                                 </Form.Group>
                             </Col>
